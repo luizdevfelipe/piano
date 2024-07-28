03659f0cca21be214616aed88ba3f19ab5222de2
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2024-07-27 21:39:55 -0300

    Função que toca músicas
    Alterada a maneira de detectar o clique.
    Função que toca a música de um array nota por nota incluindo estilo na tecla pressionada.

--- script.js
+++ script.js
@@ -1,4 +1,53 @@
-function tocar(nota){
+const fur_elise = [
+    'e5', 'd5p', 'e5', 'd5p', 'e5', 'b4', 'd5', 'c5',
+    'a4', 'c4', 'e4', 'a4', 'b4', 'e4', 'g4p', 'b4', 'c5', 'e4',
+    'e5', 'd5p', 'e5', 'd5p', 'e5', 'b4', 'd5', 'c5',
+    'a4', 'c4', 'e4', 'a4', 'b4', 'e4', 'g4p', 'b4', 'c5', 'e4',
+    'e5', 'd5p', 'e5', 'd5p', 'e5', 'b4', 'd5', 'c5',
+    'a4', 'c4', 'e4', 'a4', 'b4', 'e4', 'g4p', 'b4', 'c5', 'e4'
+]
+
+const times = [
+    400, 400, 400, 400, 400, 400, 400, 400, 400, 400, 400,
+    400, 400, 400, 400, 400, 400, 400, 400, 400,
+    400, 400, 400, 400, 400, 400, 400, 400, 400, 400, 400,
+    400, 400, 400, 400, 400, 400, 400, 400, 400,
+    400, 400, 400, 400, 400, 400, 400, 400, 400, 400, 400,
+    400, 400, 400, 400, 400, 400, 400, 400, 400, 400, 400,
+]
+
+function tocar(nota) {
     audio = new Audio(`audios/${nota}.wav`);
     audio.play();
 }
+
+async function tocar_musica() {
+
+    for (let i = 0; i < fur_elise.length; i++) {
+        const tecla = document.querySelector(`[data-nota=${fur_elise[i]}]`)
+        const classes = tecla.parentNode.classList;
+        var background = null;
+
+        classes == 'brancas' ? background = '#bebebe' : tecla.style.boxShadow = '0px 0px 7px 1px black';
+
+        if (background !== null) { tecla.style.background = background; }
+
+        tocar(fur_elise[i]);
+
+        await new Promise(resolve => setTimeout(resolve, times[i]));
+
+        background !== null ? tecla.style.background = '' : tecla.style.boxShadow = '';
+    }
+}
+
+
+window.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('ul.pretas').addEventListener('click', function (event) {
+        if (nota = event.target.closest('li').getAttribute('data-nota')) tocar(nota)
+    })
+
+    document.querySelector('ul.brancas').addEventListener('click', function (event) {
+        if (nota = event.target.closest('li').getAttribute('data-nota')) tocar(nota)
+    })
+
+})
